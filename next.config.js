81cbe300a03680e3433726eef3bd04d6f21788d6
gufronnakaaw <gufronnakaaw@gmail.com>
2021-09-14 17:38:20 +0700

    rewrite url

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites(){
+    return [
+      {
+        source: '/login',
+        destination: '/auth/login'
+      },
+      {
+        source: '/register',
+        destination: '/auth/register'
+      },
+      {
+        source: '/profile',
+        destination: '/user/profile'
+      }
+    ]
+  }
 }
