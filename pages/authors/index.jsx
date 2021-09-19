cb7822368019fca8187b94cdfb95aa0bbda14b34
gufronnakaaw <gufronnakaaw@gmail.com>
2021-09-19 10:36:49 +0700

    display authors card

--- pages/authors/index.jsx
+++ pages/authors/index.jsx
@@ -1,9 +1,33 @@
+import AuthorsCard from "../../components/AuthorsCard"
 import Layout from "../../components/Layout"
 
-export default function Authors() {
+export default function Authors({ users }) {
+    console.log(users)
     return (
-        <Layout title="Authors">
-            <h1>Authors</h1>
+        <Layout title="Authors Page">
+            <section id="hero-authors">
+                <p>Authors</p>
+            </section>
+            <div className="container mt-5">
+                <div className="row">
+                    <AuthorsCard />
+                    <AuthorsCard />
+                    <AuthorsCard />
+                    <AuthorsCard />
+                    <AuthorsCard />
+                    <AuthorsCard />
+                </div>
+            </div>
         </Layout>
     )
+}
+
+export async function getServerSideProps(){
+    const users = await fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()).then(result => result).catch(() => console.log('error cok'))
+
+    return {
+        props: {
+            users
+        }
+    }
 }
